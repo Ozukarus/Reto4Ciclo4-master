4bc10a9b2fc66fc4771bb785f3cb832649bba35f
Ozukarus <Ozukarus@OSCAR.GRANADOS2.MT@correo.usa.edu.co>
2021-12-18 00:26:58 -0500

    comentario

--- src/main/java/Reto2_web/modelo/Supplements.java
+++ src/main/java/Reto2_web/modelo/Supplements.java
@@ -8,10 +8,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
-/**
- *
- * @author Fabio Ortiz
- */
+
 
 
 @Data
@@ -23,7 +20,7 @@ public class Supplements {
 private String reference;
 private String brand;
 private String category;
-private String objetivo;
+private String material;
 private String description;
 private boolean availability = true;
 private double price;
